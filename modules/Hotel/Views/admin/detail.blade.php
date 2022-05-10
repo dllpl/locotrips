ad17cfdfdc871fef7c90a7cde213430e3873fdae
Nikita Ivanov <therockyholl@gmail.com>
2022-05-10 12:12:57 +0300

    Добавление dnd в админскую панель админа

--- modules/Hotel/Views/admin/detail.blade.php
+++ modules/Hotel/Views/admin/detail.blade.php
@@ -162,4 +162,19 @@
             });
         })
     </script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+    <script>
+        $('.attach-demo').sortable({
+            items: '.image-item',
+            opacity: 0.7,
+            axis: 'x',
+            update: function() {
+                var arr = [];
+                $('.image-item a').each(function (i, e) {
+                    arr.push($(e).attr('data-id'))
+                });
+                $('#gallery_input').val(arr)
+            }
+        })
+    </script>
 @endsection
