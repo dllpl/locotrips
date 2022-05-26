1c863c80f512d51391c521aefbf3f8772aa7b05f
Nikita Ivanov <nikita03.1998@mail.ru>
2022-05-26 10:38:58 +0300

    Поиск в алфавите

--- modules/Car/Views/admin/car/location.blade.php
+++ modules/Car/Views/admin/car/location.blade.php
@@ -24,6 +24,25 @@
                                 }
                             };
                             $traverse($car_location);
+
+                            function array_orderby()
+                            {
+                                $args = func_get_args();
+                                $data = array_shift($args);
+                                foreach ($args as $n => $field) {
+                                    if (is_string($field)) {
+                                        $tmp = array();
+                                        foreach ($data as $key => $row)
+                                            $tmp[$key] = $row[$field];
+                                        $args[$n] = $tmp;
+                                    }
+                                }
+                                $args[] = &$data;
+                                call_user_func_array('array_multisort', $args);
+                                return array_pop($args);
+                            }
+                            $list_json = array_orderby($list_json, 'title', SORT_ASC);
+
                             ?>
                             <div class="smart-search">
                                 <input type="text" class="smart-search-location parent_text form-control" placeholder="{{__("-- Please Select --")}}" value="{{ $location_name }}" data-onLoad="{{__("Loading...")}}"
